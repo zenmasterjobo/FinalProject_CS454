06da2ad300e0b15338912ab8092d620db5f35c3a
Jordan Bergero <bergero@sonoma.edu>
2016-10-24 18:11:22 -0700

    trimming the plants -JB

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,21 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
+import cv2
+import numpy
+
+
+
+
+def state():
+
+
+
+
+def edges():
+
+
+
+
+############## Picture shit I am not really sure. Who knows what the fuck openCV is capable of, ######x
 
 
 def main():
